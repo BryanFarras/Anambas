954203e633679a07a24bc736b4d8dae6571ca483
LAPTOP-FUT766KP\asus <farrasbryan06@gmail.com>
2026-03-14 21:21:09 +0700

    water tweek

--- scripts/audio/sfx_character.gd
+++ scripts/audio/sfx_character.gd
@@ -2,8 +2,21 @@ extends AudioStreamPlayer2D
 
 @export var steps_sand: Array[AudioStream] = [null, null, null, null]
 @export var steps_soil: Array[AudioStream] = [null, null, null, null]
-@export var steps_wood: Array[AudioStream] = [null, null, null, null]
+@export var steps_wood: Array[AudioStream] = [null, null, null, null] 
+@export var steps_grass: Array[AudioStream] = [null, null, null, null] 
 
+@export var footstep_interval: float = 0.4
+
+var _current_surface: String = ""
+var _is_walking: bool = false
+var _footstep_timer: Timer
+
+func _ready() -> void:
+	_footstep_timer = Timer.new()
+	_footstep_timer.wait_time = footstep_interval
+	_footstep_timer.one_shot = true
+	_footstep_timer.timeout.connect(_on_footstep_timer_timeout)
+	add_child(_footstep_timer)
 func play_footstep(surface_type: String) -> void:
 	var clips: Array[AudioStream] = []
 	
@@ -22,5 +35,31 @@ func play_footstep(surface_type: String) -> void:
 	if valid_clips.is_empty():
 		return
 		
+	_current_surface = surface_type
+	_is_walking = true
+	
+	if not playing and _footstep_timer.is_stopped():
+		_play_random_clip(valid_clips)
+
+func stop_footstep() -> void:
+	_is_walking = false
+	_current_surface = ""
+	_footstep_timer.stop()
+	stop()
+
+func _play_random_clip(valid_clips: Array[AudioStream]) -> void:
 	stream = valid_clips.pick_random()
 	play()
+
+func _on_footstep_timer_timeout() -> void:
+	if _is_walking and _current_surface != "":
+		play_footstep(_current_surface)
+
+# Connect to the built-in finished signal to trigger the timer
+func _on_finished() -> void:
+	if _is_walking:
+		_footstep_timer.start(footstep_interval)
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_POSTINITIALIZE:
+		finished.connect(_on_finished)
